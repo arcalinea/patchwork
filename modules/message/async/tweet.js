8d300ff9f5f7db4dda557e259798f15e458caf54
Jay Graber <jay@z.cash>
2018-05-10 11:38:17 -0700

    Add tweet.js to hit twitter API

---
+++ modules/message/async/tweet.js
@@ -0,0 +1,50 @@
+var h = require('mutant/h')
+var nest = require('depnest')
+var twit = require('twit');
+
+var config = require('../../../config.js');
+
+exports.needs = nest({
+  'sheet.display': 'first',
+  'message.html.render': 'first',
+  'keys.sync.id': 'first',
+  'intl.sync.i18n': 'first'
+})
+
+exports.gives = nest('message.async.tweet')
+
+exports.create = function (api) {
+  const i18n = api.intl.sync.i18n
+  const Twitter = new twit(config);
+
+  return nest('message.async.tweet', function (content, cb) {
+    api.sheet.display(function (close) {
+        return {
+          content: [
+            api.message.html.render({value: {
+              content,
+              private: !!content.recps,
+              author: api.keys.sync.id()
+            }})
+          ],
+          footer: [
+            h('button -save', { 'ev-click': tweet }, i18n('Confirm Tweet')),
+            h('button -cancel', { 'ev-click': cancel }, i18n('Cancel'))
+          ]
+        }
+        
+      function tweet () {
+        close()
+        Twitter.post('statuses/update', { status: content.text }, function(err, data, response) {
+          console.log(data)
+        })
+      }
+
+      function cancel () {
+        close()
+        cb && cb(null, false)
+      }
+    })
+    return true
+  })
+}
